08be2143f0c69c12f17e8ce895e38fd69c0f1e65
tomwie <tomekwielek@o2.pl>
2018-10-12 12:39:01 +0200

    'Add'

--- main.py
+++ main.py
@@ -2,9 +2,7 @@ import os
 import numpy as np
 from config import myload, base_path, paths
 from functional import (select_class_to_classif, cat_pe_stag, load_single_append,
-                        vis_PCA, classify_lso, classify_generalize_time,
-                        merge_stages, count_stag, group_h0, vis_clf_probas,
-                        align_t1_t2_data, write_pickle, read_pickle, plot_confusion_matrix)
+                        count_stag, vis_clf_probas, write_pickle, read_pickle, plot_confusion_matrix)
 from matplotlib import pyplot as plt
 from read_raw import *
 from mne import io
@@ -12,7 +10,6 @@ import pandas as pd
 import pickle
 import seaborn as sns
 from classify_with_shuffling_v2 import classify_shuffle
-from classify_with_shuffling_v2_crosstime import classify_shuffle_crosstime
 from collections import defaultdict
 from IPython.core.debugger import set_trace
 
@@ -151,45 +148,3 @@ print 'pv acc {}, \n pv recall {},\n pv precision {}\n'.format(p_acc, p_r, p_p)
 #write_pickle(res, 'perf_nulldistr2.txt')
 #dd1 = read_pickle('perf_nulldistr1.txt')
 #dd2 = read_pickle('sign_t2_mspe.txt')
-
-################################################################################
-# TIME GENERALIZATION BASED ON MSPE
-'''
-mspe1, stag1, names1, _ = load_single_append(path, fnames1, typ=setup)
-mspe2, stag2, names2, _ = load_single_append(path, fnames2, typ=setup)
-mspe1, stag1 = select_class_to_classif(mspe1, stag1, sel_idxs=sel_idxs)
-mspe2, stag2 = select_class_to_classif(mspe2, stag2, sel_idxs=sel_idxs)
-
-mspe1, mspe2, stag1, stag2 = align_t1_t2_data(mspe1, mspe2, stag1, stag2) #get matches only
-
-mspe1_ = [ mspe1[i ][:4, ...] for i in range(len(mspe1)) ]  #use scale: 1, 2, 3, 4 only
-mspe1 = [ mspe1_[i].reshape(-1, mspe1_[i].shape[-1]) for i in range(len(mspe1_)) ] # reshape
-
-mspe2_ = [ mspe2[i ][:4, ...] for i in range(len(mspe2)) ] #use scale: 1, 2, 3, 4 only
-mspe2 = [ mspe2_[i].reshape(-1, mspe2_[i].shape[-1]) for i in range(len(mspe2_)) ] #reshape
-early2late = True
-perf = classify_shuffle_crosstime(mspe1, mspe2, stag1, stag2, myshow=False, \
-                    check_mspe=True, null=False, early2late=early2late)
-print np.asarray(perf).mean()
-##
-#permutation for signle subject
-nulliter  = 100
-nullperf = np.empty([len(perf), nulliter])
-for i in range(nulliter):
-    nullperf[:,i] = classify_shuffle_crosstime(mspe1, mspe2, stag1, stag2, myshow=False, \
-                                    check_mspe=True, null=True, early2late=early2late)
-    print i
-
-#bootstrapping for null on a group level
-nullgroupiter = 1000
-nullperf_gr = np.zeros([nullgroupiter, 1])
-for j in range(nullgroupiter):
-    # sample with repl. subject performance and average
-    nullperf_gr[j, :] = np.mean(nullperf[:, np.random.choice(range(nulliter))])
-
-    r = nullperf_gr > np.asarray(perf).mean()
-    #p = r.sum() / float(len(h0_gr))
-    p = r.sum() / ( float(nullgroupiter) + 1 )
-res = [perf, nullperf_gr, p]
-write_pickle(res, 'sign_t1t2_mspe_weightedF1.txt')
-'''
