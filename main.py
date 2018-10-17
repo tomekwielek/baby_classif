9ba1612f7cb716b4a67ea09ee8bc84bf167591fd
tomwie <tomekwielek@o2.pl>
2018-10-17 09:18:43 +0200

    'Upd'

--- main.py
+++ main.py
@@ -29,17 +29,17 @@ store_acc = {'mspet1m3' : [], 'psd':[]}
 for setup in ['mspet1m3', 'psd']:
     for i in range(20):
         if setup == 'mspet1m3':
-        # SIMPLE GENERALIZATION BASED ON MSPE
-        mspe1, stag1, names1, _ = load_single_append(path, fnames1, typ=setup)
-        mspe2, stag2, names2, _ = load_single_append(path, fnames2, typ=setup)
-        mspe1, stag1 = select_class_to_classif(mspe1, stag1, sel_idxs=sel_idxs)
-        mspe2, stag2 = select_class_to_classif(mspe2, stag2, sel_idxs=sel_idxs)
+            # SIMPLE GENERALIZATION BASED ON MSPE
+            mspe1, stag1, names1, _ = load_single_append(path, fnames1, typ=setup)
+            mspe2, stag2, names2, _ = load_single_append(path, fnames2, typ=setup)
+            mspe1, stag1 = select_class_to_classif(mspe1, stag1, sel_idxs=sel_idxs)
+            mspe2, stag2 = select_class_to_classif(mspe2, stag2, sel_idxs=sel_idxs)
 
-        mspe1_ = [ mspe1[i ][:4, ...] for i in range(len(mspe1)) ]  #use scale: 1, 2, 3, 4 only
-        mspe1 = [ mspe1_[i].reshape(-1, mspe1_[i].shape[-1]) for i in range(len(mspe1_)) ] # reshape
+            mspe1_ = [ mspe1[i ][:4, ...] for i in range(len(mspe1)) ]  #use scale: 1, 2, 3, 4 only
+            mspe1 = [ mspe1_[i].reshape(-1, mspe1_[i].shape[-1]) for i in range(len(mspe1_)) ] # reshape
 
-        mspe2_ = [ mspe2[i ][:4, ...] for i in range(len(mspe2)) ] #use scale: 1, 2, 3, 4 only
-        mspe2 = [ mspe2_[i].reshape(-1, mspe2_[i].shape[-1]) for i in range(len(mspe2_)) ] #reshape
+            mspe2_ = [ mspe2[i ][:4, ...] for i in range(len(mspe2)) ] #use scale: 1, 2, 3, 4 only
+            mspe2 = [ mspe2_[i].reshape(-1, mspe2_[i].shape[-1]) for i in range(len(mspe2_)) ] #reshape
 
         if time == 2:
             data_pe, data_stag = mspe1, stag1
@@ -50,20 +50,20 @@ for setup in ['mspet1m3', 'psd']:
             data_stag = stag1 + stag2
 
         elif setup == 'psd':
-        # SIMPLE GENERALIZATION BASED ON PSD
-        psd1, stag1, names1, freqs = load_single_append(path, fnames1, typ=setup)
-        psd2, stag2, names2, freqs = load_single_append(path, fnames2, typ=setup)
-        psd1, stag1 = select_class_to_classif(psd1, stag1, sel_idxs=sel_idxs)
-        psd2, stag2 = select_class_to_classif(psd2, stag2, sel_idxs=sel_idxs)
+            # SIMPLE GENERALIZATION BASED ON PSD
+            psd1, stag1, names1, freqs = load_single_append(path, fnames1, typ=setup)
+            psd2, stag2, names2, freqs = load_single_append(path, fnames2, typ=setup)
+            psd1, stag1 = select_class_to_classif(psd1, stag1, sel_idxs=sel_idxs)
+            psd2, stag2 = select_class_to_classif(psd2, stag2, sel_idxs=sel_idxs)
 
-        rel_psd1 = [psd1[i] / np.abs(np.sum(psd1[i], 0)) for i in range(len(psd1))]
-        rel_psd1 = [ np.log10(rel_psd1[i]) for i in range(len(psd1)) ]
-        rel_psd2 = [psd2[i] / np.abs(np.sum(psd2[i], 0)) for i in range(len(psd2))]
-        rel_psd2 = [ np.log10(rel_psd2[i]) for i in range(len(psd2)) ]
-        psd1 = rel_psd1
-        psd2 = rel_psd2
-        psd1 = [ psd1[i].reshape(-1, psd1[i].shape[-1]) for i in range(len(psd1)) ] # reshape
-        psd2 = [ psd2[i].reshape(-1, psd2[i].shape[-1]) for i in range(len(psd2)) ] #reshape
+            rel_psd1 = [psd1[i] / np.abs(np.sum(psd1[i], 0)) for i in range(len(psd1))]
+            rel_psd1 = [ np.log10(rel_psd1[i]) for i in range(len(psd1)) ]
+            rel_psd2 = [psd2[i] / np.abs(np.sum(psd2[i], 0)) for i in range(len(psd2))]
+            rel_psd2 = [ np.log10(rel_psd2[i]) for i in range(len(psd2)) ]
+            psd1 = rel_psd1
+            psd2 = rel_psd2
+            psd1 = [ psd1[i].reshape(-1, psd1[i].shape[-1]) for i in range(len(psd1)) ] # reshape
+            psd2 = [ psd2[i].reshape(-1, psd2[i].shape[-1]) for i in range(len(psd2)) ] #reshape
         if time == 2:
             data_pe, data_stag = psd1, stag1
         elif time == 5:
